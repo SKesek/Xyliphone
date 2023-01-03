197ab8ca8231811230d7c58a7e39026250968b50
Krzysztof Swend <krzysiekswend@gmail.com>
2023-01-03 15:48:14 +0100

    project completed
    add function to play sound
    add function to create new TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey(Color colorButton, int noteNumber) {
+    return Expanded(
+        child: Container(
+            color: colorButton,
+            child: TextButton(
+              onPressed: () {
+                playSound(noteNumber);
+              },
+              child: Text(''),
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('hello'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
           ),
         ),
       ),
